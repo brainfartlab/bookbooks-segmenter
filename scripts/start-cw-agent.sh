9474d5d47b7e21a1ecc914ffba6ab599405e031b
Antoine Vandermeersch <avdmeers@gmail.com>
2023-11-08 14:02:58 +0100

    bugfix for CW agent config

--- scripts/start-cw-agent.sh
+++ scripts/start-cw-agent.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo cp ./config/cw-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/segmenter/config/cw-config.json -s
