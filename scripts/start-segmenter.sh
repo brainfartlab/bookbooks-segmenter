829c3f6bfc1786005c20009ef611c3dd08549a29
Antoine Vandermeersch <avdmeers@gmail.com>
2023-11-09 11:49:07 +0100

    activate pytorch for segmenter

--- scripts/start-segmenter.sh
+++ scripts/start-segmenter.sh
@@ -6,4 +6,5 @@ for s in $(echo $values | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|
 done
 
 echo "$RAW_QUEUE_URL" >> /var/opt/bookbooks/segmenter.log
+source activate pytorch
 segmenter >> /var/opt/bookbooks/segmenter.log
