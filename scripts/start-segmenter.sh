431d40e76296c75b9cb11ffc90ae92f58d960d57
Antoine Vandermeersch <avdmeers@gmail.com>
2023-11-14 11:51:18 +0100

    enable segmenter in CodeDeploy upon start

--- scripts/start-segmenter.sh
+++ scripts/start-segmenter.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+# Application variables from SSM
 values=$(aws ssm --region eu-west-1 get-parameter --name "/bookbooks/segmenter/config" | jq -r '.Parameter.Value')
 
 for s in $(echo $values | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
     export $s
 done
 
-echo "$RAW_QUEUE_URL" >> /var/opt/bookbooks/segmenter.log
+# Infrastructure variables from instance metadata
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region)
+
+export AWS_DEFAULT_REGION=$AWS_REGION
+
 source activate pytorch
-#segmenter --input-queue $RAW_QUEUE_URL >> /var/opt/bookbooks/segmenter.log
+segmenter \
+  -i $RAW_QUEUE_URL \
+  -o $SEGMENTS_QUEUE_URL \
+  -b $IMAGE_BUCKET \
+  >> /var/opt/bookbooks/segmenter.log
